cd8a53e8119c558a94c8877e7c9a004fbfe292fc
Vilma <vilmail@protonmail.com>
2023-02-11 23:15:48 +0200

    added extra functionality to test if args 0, NaN, Infinity

--- exercises/01.js
+++ exercises/01.js
@@ -1,9 +1,10 @@
 /**
  *
- * @param {number} y - rectangular vertical side length, positive number
- * @param {number} x - rectangular horizontal side length, positive number
+ * @param {number} y - rectangular vertical side length, positive finite number more than 0
+ * @param {number} x - rectangular horizontal side length, positive finite number more than 0
  * @returns {Array} array with 2 elements: boolean - showing invalid argument and (string - failure message or number - calculated rectangular area)
  */
+// No checks for too big numbers. What precision to return area if floating point number?
 function rectangularShapeArea(y, x) {
   return checkIfInvalidArg(y) || checkIfInvalidArg(x) || [false, y * x];
 
@@ -12,13 +13,12 @@ function rectangularShapeArea(y, x) {
     if (type !== "number") {
       return [true, `Negali buti "${arg === null ? "null" : type}"`];
     }
-    if (arg < 0) {
-      return [true, "Negali buti neigiamas"];
+    if (arg <= 0 || !Number.isFinite(arg)) {
+      return [true, `Negali buti ${arg < 0 ? "neigiamas" : arg.toString()}`];
     }
     return false;
   }
 }
-
 console.log(rectangularShapeArea(-10, 10), "-->", [true, "Negali buti neigiamas"]);
 console.log(rectangularShapeArea(10, -10), "-->", [true, "Negali buti neigiamas"]);
 console.log(rectangularShapeArea(10, 10), "-->", [false, 100]);
@@ -40,3 +40,6 @@ console.log(rectangularShapeArea(10), "-->", [true, `Negali buti "undefined"`]);
 console.log(rectangularShapeArea(undefined, 10), "-->", [true, `Negali buti "undefined"`]);
 console.log(rectangularShapeArea(10, null), "-->", [true, `Negali buti "null"`]);
 console.log(rectangularShapeArea(null, 10), "-->", [true, `Negali buti "null"`]);
+console.log(rectangularShapeArea(Infinity, 10), "-->", [true, `Negali buti Infinity`]);
+console.log(rectangularShapeArea(10, -Infinity), "-->", [true, `Negali buti neigiamas`]);
+console.log(rectangularShapeArea(10, NaN), "-->", [true, `Negali buti NaN`]);
