25bcb4df5fb8ccbc20aedfbb5d68ca0143272933
Vilma <vilmail@protonmail.com>
2023-02-12 17:58:49 +0200

    TASK 3 initial commit

--- exercises/03.js
+++ exercises/03.js
@@ -1,45 +1,45 @@
-const assert = require('assert');
+const assert = require("assert");
 
 console.clear();
 
 function fac1(list) {
-    return list;
+  return list;
 }
 
 function fac2(list) {
-    return list.map(skaicius => {
-        return skaicius * 2;
-    });
+  return list.map((skaicius) => {
+    return skaicius * 2;
+  });
 }
 
 function fac3(list) {
-    var new_grand = [];
-    let i = 0;
-    while (i < list.length) {
-        new_grand.push(list[i++] * 3);
-    }
-    return new_grand;
+  var new_grand = [];
+  let i = 0;
+  while (i < list.length) {
+    new_grand.push(list[i++] * 3);
+  }
+  return new_grand;
 }
 
 function f_a_c_t_o_r_y(skaiciu_grandinele = [], double = 1) {
-    let fac = null;
+  let fac = null;
 
-    if (double == 1) {
-        fac = fac1;
-    } else if (double == 2) {
-        fac = fac2;
-    } else if (double === 3) {
-        fac = fac3;
-    } else {
-        // FIX:
-        // not sure how, but there has to be a way
-        // to call a single EXTERNAL FUCNTION with 
-        // any "double" value, so that we would not
-        // need to call infinite amount of else-if's
-    }
+  if (double == 1) {
+    fac = fac1;
+  } else if (double == 2) {
+    fac = fac2;
+  } else if (double === 3) {
+    fac = fac3;
+  } else {
+    // FIX:
+    // not sure how, but there has to be a way
+    // to call a single EXTERNAL FUCNTION with
+    // any "double" value, so that we would not
+    // need to call infinite amount of else-if's
+  }
 
-    // DO NOT EDIT THIS LINE
-    return fac(skaiciu_grandinele);
+  // DO NOT EDIT THIS LINE
+  return fac(skaiciu_grandinele);
 }
 
 const factory = f_a_c_t_o_r_y;
