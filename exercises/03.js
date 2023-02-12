8be34a668f248f1afed903663a8d02e2f6128d4b
Vilma <vilmail@protonmail.com>
2023-02-12 20:03:26 +0200

    calling one external function

--- exercises/03.js
+++ exercises/03.js
@@ -1,45 +1,18 @@
 const assert = require("assert");
 
 console.clear();
-function fac1(list) {
-  return list;
-}
 
-function fac2(list) {
-  return list.map((skaicius) => {
-    return skaicius * 2;
-  });
-}
-
-function fac3(list) {
-  //this is doing same as fac2 with map method
-  var new_grand = [];
-  let i = 0;
-  while (i < list.length) {
-    new_grand.push(list[i++] * 3);
-  }
-  return new_grand;
+function multiplyVals(multiplier) {
+  return (arr) => {
+    if (multiplier === 1) {
+      return arr;
+    }
+    return arr.map((num) => num * multiplier);
+  };
 }
 
 function arrayValuesMultiplier(numbersArray = [], multiplier = 1) {
-  let fac = null;
-
-  if (multiplier == 1) {
-    fac = fac1;
-  } else if (multiplier == 2) {
-    fac = fac2;
-  } else if (multiplier === 3) {
-    fac = fac3;
-  } else {
-    // return;
-    return numbersArray.map((number) => number * multiplier); //just temp general to stop errors
-    // FIX:
-    // not sure how, but there has to be a way
-    // to call a single EXTERNAL FUCNTION with
-    // any "double" value, so that we would not
-    // need to call infinite amount of else-if's
-    //-- Not sury why to call external when everything can be done there... But ok..
-  }
+  let fac = multiplyVals(multiplier);
 
   // DO NOT EDIT THIS LINE
   return fac(numbersArray);
