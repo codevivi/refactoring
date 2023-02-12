445e34842a637264a5a8e19092fe2f3a2ff9f7b3
Vilma <vilmail@protonmail.com>
2023-02-12 13:07:51 +0200

    some rename to remove capitalized let or to make more descriptive, var swap to let as all of them used in a block anyway

--- exercises/02.js
+++ exercises/02.js
@@ -1,80 +1,79 @@
 console.clear();
 /**
  * Should print notification for each student.
- * @param {*} list 
+ * @param {*} list
  */
 function marksAverages_forEvery_student(list) {
-    const notifications = [];
-    for (var j = list.length; j != 0;) {
-        var stud = list[list.length - j--];
+  const notifications = [];
+  for (let j = list.length; j != 0; ) {
+    let stud = list[list.length - j--];
 
-        if (stud['name'] == 'Maryte') {
-            let i = 0;
-            var maryteAverage = 0;
-            for (; i < stud['marks'].length; i++) {
-                // should be some sort of validation...
-                // can not remember what...
-                if (true) {
-                    let m = stud['marks'][i];
-                    maryteAverage += m;
-                }
-            }
-            maryteAverage /= stud['marks'].length;
-            // console.log(`${stud['name']} mark average is ${maryteAverage}.`);
-            notifications.push(`${stud['name']} mark average is ${maryteAverage}.`);
+    if (stud["name"] == "Maryte") {
+      let i = 0;
+      let sum = 0;
+      for (; i < stud["marks"].length; i++) {
+        // should be some sort of validation...
+        // can not remember what...
+        if (true) {
+          let m = stud["marks"][i];
+          sum += m;
         }
-
-        if (stud.name === 'Jonas') {
-            let Count = stud.marks.length;
-            let Sum = 0;
-            while (Count) {
-                if (stud.marks[--Count] <= 10 && stud.marks[Count] % 1 == 0 && stud.marks[Count] > 0) {
-                    Sum = Sum + stud.marks[Count];
-                }
-            }
-            // console.log(`${stud.name} mark average is ${Sum / 4}.`);
-            notifications.push(`${stud.name} mark average is ${Sum / 4}.`);
-        }
-
-        switch (stud.name) {
-            case 'Petras':
-                notifications.push(`${stud.name} mark average is ${stud.marks.reduce((t, m) => t + m) / stud.marks.length}.`);
-                // console.log(`${stud.name} mark average is ${stud.marks.reduce((t, m) => t + m) / stud.marks.length}.`);
-                break;
-
-            default:
-                let s = 0;
-                for (const m of stud['marks']) {
-                    s += m / stud.marks.length;
-                }
-                notifications.push(`${stud.name} mark average is ${s}.`);
-                // console.log(`${stud.name} mark average is ${s}.`);
-                break;
-        }
+      }
+      let average = sum / stud["marks"].length;
+      // console.log(`${stud['name']} mark average is ${average}.`);
+      notifications.push(`${stud["name"]} mark average is ${average}.`);
     }
 
-    return notifications;
+    if (stud.name === "Jonas") {
+      let count = stud.marks.length;
+      let sum = 0;
+      while (count) {
+        if (stud.marks[--count] <= 10 && stud.marks[count] % 1 == 0 && stud.marks[count] > 0) {
+          sum = sum + stud.marks[count];
+        }
+      }
+      // console.log(`${stud.name} mark average is ${sum / 4}.`);
+      notifications.push(`${stud.name} mark average is ${sum / 4}.`);
+    }
+
+    switch (stud.name) {
+      case "Petras":
+        notifications.push(`${stud.name} mark average is ${stud.marks.reduce((t, m) => t + m) / stud.marks.length}.`);
+        // console.log(`${stud.name} mark average is ${stud.marks.reduce((t, m) => t + m) / stud.marks.length}.`);
+        break;
+
+      default:
+        let average = 0;
+        for (const m of stud["marks"]) {
+          average += m / stud.marks.length;
+        }
+        notifications.push(`${stud.name} mark average is ${average}.`);
+        // console.log(`${stud.name} mark average is ${s}.`);
+        break;
+    }
+  }
+
+  return notifications;
 }
 
 const student = [
-    {
-        name: 'Petras',
-        marks: [10, 2, 8, 4, 6],
-    },
-    {
-        name: 'Maryte',
-        marks: [4, 6, 8, 10],
-    },
-    {
-        name: 'Jonas',
-        marks: [8, 7, 5, 4, -1, 13, 8.7],
-    },
-    {
-        name: 'Ona',
-        marks: [],
-    },
+  {
+    name: "Petras",
+    marks: [10, 2, 8, 4, 6],
+  },
+  {
+    name: "Maryte",
+    marks: [4, 6, 8, 10],
+  },
+  {
+    name: "Jonas",
+    marks: [8, 7, 5, 4, -1, 13, 8.7],
+  },
+  {
+    name: "Ona",
+    marks: [],
+  },
 ];
-
 
 const results = marksAverages_forEvery_student(student);
 
@@ -82,10 +81,10 @@ const results = marksAverages_forEvery_student(student);
 // DO NOT EDIT CODE BELOW THIS COMMENT
 // ***********************************
 
-console.assert(results[0] === 'Petras mark average is 6.', 'Neteisingai suskaiciuotas Petro pazymiu vidurkis.');
+console.assert(results[0] === "Petras mark average is 6.", "Neteisingai suskaiciuotas Petro pazymiu vidurkis.");
 
-console.assert(results[1] === 'Maryte mark average is 7.', 'Neteisingai suskaiciuotas Marytes pazymiu vidurkis.');
+console.assert(results[1] === "Maryte mark average is 7.", "Neteisingai suskaiciuotas Marytes pazymiu vidurkis.");
 
-console.assert(results[2] === 'Jonas mark average is 6.', 'Neteisingai suskaiciuotas Jono pazymiu vidurkis.');
+console.assert(results[2] === "Jonas mark average is 6.", "Neteisingai suskaiciuotas Jono pazymiu vidurkis.");
 
-console.assert(results[3] === 'Ona does not have any marks yet.', 'Neteisingai suskaiciuotas Onos pazymiu vidurkis.');
+console.assert(results[3] === "Ona does not have any marks yet.", "Neteisingai suskaiciuotas Onos pazymiu vidurkis.");
